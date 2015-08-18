608333a294e12abde3dbbbf6298b105f1aa63526
Ben Murphy <benmmurphy@gmail.com>
2015-08-18 17:13:11 +0100

    fix project url

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/fonixcode/zensend_python_api',
+    url='https://github.com/zensend/zensend_python_api',
 
     # Author details
     author='ZenSend',
